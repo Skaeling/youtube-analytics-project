a2f5c685a77dfaea43bd4b896257f5e459b339a1
Lana Virina <lana.virina@gmail.com>
2023-03-17 00:22:24 +0300

    Add magic methods: str, add, sub, ge in Channel

--- src/channel.py
+++ src/channel.py
@@ -16,9 +16,26 @@ class Channel:
         self.title = self.channel['items'][0]['snippet']['title']
         self.description = self.channel['items'][0]['snippet']['description']
         self.url = self.channel['items'][0]['snippet']['thumbnails']['default']['url']
-        self.subscribers = self.channel['items'][0]['statistics']['subscriberCount']
+        self.subscribers = int(self.channel['items'][0]['statistics']['subscriberCount'])
         self.video_count = self.channel['items'][0]['statistics']['videoCount']
         self.view_count = self.channel['items'][0]['statistics']['viewCount']
+
+    def __str__(self):
+        """Возвращает название и ссылку на канал
+        по шаблону `<название_канала> (<ссылка_на_канал>)`"""
+        return f"{self.title} ({self.url})"
+
+    def __add__(self, other):
+        """Реализует возможность складывать два канала между собой по количеству подписчиков"""
+        return self.subscribers + other.subscribers
+
+    def __sub__(self, other):
+        """Реализует возможность вычитать количество подписчиков одного канала из другого"""
+        return self.subscribers - other.subscribers
+
+    def __ge__(self, other):
+        """Реализует возможность сравнивать два канала между собой по количеству подписчиков"""
+        return self.subscribers >= other.subscribers
 
     @property
     def channel_id(self):
